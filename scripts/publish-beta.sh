cc99e095359c79fd09e40ffc80d9567bb61fa43e
Rajan Tripathi <tripathirajan3@gmail.com>
2025-07-07 09:01:18 +0530

    chore: updated package version

--- scripts/publish-beta.sh
+++ scripts/publish-beta.sh
@@ -9,12 +9,19 @@ npm run build
 echo "🔖 Bumping beta version..."
 npm version prerelease --preid=beta
 
+# Get new version
+NEW_VERSION=$(node -p "require('./package.json').version")
+echo "📦 New version: $NEW_VERSION"
+
 # Publish to npm
 echo "🚀 Publishing to npm (beta)..."
 npm publish --tag beta --access public
 
-# Push Git tags
+# Update dist-tag to point beta to the latest beta version
+echo "🏷️ Updating dist-tag 'beta' to $NEW_VERSION..."
+npm dist-tag add @tripathirajan/fetcher@$NEW_VERSION beta
 
+# Push Git tags
 echo "🔀 Pushing Git tags..."
 git push origin --tags
 
